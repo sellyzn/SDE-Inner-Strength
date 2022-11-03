9fdcf0497e193417b70f5001210ea4a431d0b2f8
NingZhang <selenazhang@outlook.com>
2022-11-03 03:58:35 -0700

    add LeetCodeStudyPlan and TopMicrosoftQuestions

---
+++ TopMicrosoftQuestions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TopMicrosoftQuestions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
